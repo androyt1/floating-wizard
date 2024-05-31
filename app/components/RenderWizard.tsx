34a8b852d8a9053c452858fce11cc6bc6fced765
Andrew Aghoghovwia <androyt1@gmail.com>
2024-05-31 09:24:42 +0100

    added loading progress

--- app/components/RenderWizard.tsx
+++ app/components/RenderWizard.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useGLTF, Environment } from "@react-three/drei";
+import { useGLTF, Environment, useProgress, Html } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 
-import { forwardRef, useEffect, useRef } from "react";
+import { forwardRef, useRef, Suspense } from "react";
 
 const Wizard = forwardRef(function MyWizard(props, ref) {
     const model = useGLTF("/models/wizard.glb");
@@ -12,13 +12,24 @@ const Wizard = forwardRef(function MyWizard(props, ref) {
     return <primitive object={model.scene} {...props} ref={ref} />;
 });
 
+function Loader() {
+    const { progress } = useProgress();
+    return (
+        <Html center className='text-white text-3xl font-semibold min-w-[400px]'>
+            <span>loading: {Math.round(progress)} % loaded</span>{" "}
+        </Html>
+    );
+}
+
 const RenderWizard = () => {
     const modelRef = useRef();
 
     return (
         <Canvas className='h-screen'>
-            <Wizard scale={0.05} position-y={-2} ref={modelRef} />
-            <Environment preset='dawn' />
+            <Suspense fallback={<Loader />}>
+                <Wizard scale={0.05} position-y={-2} ref={modelRef} />
+                <Environment preset='dawn' />
+            </Suspense>
         </Canvas>
     );
 };
